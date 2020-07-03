547e9c0951dde4fe6101b90f816bc6f45d38f462
Nicolas Roelandt <roelandtn.pro@gmail.com>
2020-07-03 14:32:48 +0200

    Add csv extension to extracted data

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -4,10 +4,16 @@ tweets_url <- "https://sharedocs.huma-num.fr/wl/?id=vMQPwEMY5owHLmuSxlXLDJ0Q7sqO
 library(dplyr)
 library(readr)
 
+# Extract raw tweets
 raw_tweets <- readr::read_csv(file = tweets_url,
                               col_types = cols(id = "c", conversation_id = "c", created_at = "c"))
 
-
 usethis::use_data(raw_tweets, overwrite = TRUE)
 
+# Extract tweets links
 
+condensed_tweets <- raw_tweets %>%
+  select(id, link, hashtags, urls) %>% # remove empty or unused cols
+  write_csv("data-raw/condensed_tweets.csv")
+
+usethis::use_data(condensed_tweets, overwrite = TRUE)
