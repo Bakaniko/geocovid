a71cfc2b4792a64589aa9eef2cad1d6dcf6f91ea
Nicolas Roelandt <roelandtn.pro@gmail.com>
2020-07-03 14:30:38 +0200

    Extract condensed dataset

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -1,3 +1,13 @@
 ## code to prepare `DATASET` dataset goes here
+tweets_url <- "https://sharedocs.huma-num.fr/wl/?id=vMQPwEMY5owHLmuSxlXLDJ0Q7sqOWUUz"
 
-usethis::use_data(DATASET, overwrite = TRUE)
+library(dplyr)
+library(readr)
+
+raw_tweets <- readr::read_csv(file = tweets_url,
+                              col_types = cols(id = "c", conversation_id = "c", created_at = "c"))
+
+
+usethis::use_data(raw_tweets, overwrite = TRUE)
+
+
