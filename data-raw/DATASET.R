dbcf06ecfcad53dbdf339331353a1aaa7de55dac
Nicolas Roelandt <roelandtn.pro@gmail.com>
2020-07-03 14:59:47 +0200

    Clean urls strings and add tweet date

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -3,7 +3,7 @@ tweets_url <- "https://sharedocs.huma-num.fr/wl/?id=vMQPwEMY5owHLmuSxlXLDJ0Q7sqO
 
 library(dplyr)
 library(readr)
-
+library(stringr)
 # Extract raw tweets
 raw_tweets <- readr::read_csv(file = tweets_url,
                               col_types = cols(id = "c", conversation_id = "c", created_at = "c"))
@@ -13,7 +13,13 @@ usethis::use_data(raw_tweets, overwrite = TRUE)
 # Extract tweets links
 
 condensed_tweets <- raw_tweets %>%
-  select(id, link, hashtags, urls) %>% # remove empty or unused cols
-  write_csv("data-raw/condensed_tweets.csv")
+  select(id, tweet = link, urls, hashtags, date) %>% # remove empty or unused cols
+  dplyr::mutate(
+    # clean strings
+    hashtags =  hashtags %>%
+      stringr::str_replace_all("[\\['\\]]", ""),
+    urls =  urls %>%
+      stringr::str_replace_all("[\\['\\]]", "")) %>%
+  write_csv("data-raw/condensed_tweets.csv") # write in a csv
 
 usethis::use_data(condensed_tweets, overwrite = TRUE)
